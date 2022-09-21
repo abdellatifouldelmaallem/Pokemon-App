91689ef48d4bad35d56217f8c7a00d80acbfcb27
abdellatif <ouldeabdellatif40@gmail.com>
2022-09-21 18:49:17 +0100

    added for biding data pokemonService

--- Portfolio/src/app/pokemon/pokemon.service.ts
+++ Portfolio/src/app/pokemon/pokemon.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { POKEMONS } from './mock-pokemons';
+import { Pokemon } from './pokemon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  table = [];
+
+ getPokemonList(): Pokemon[]{
+     return POKEMONS;
+ }
+
+ getPokemonById(pokemonId:number): Pokemon|undefined{
+  return POKEMONS.find(pokemon=>pokemon.id == pokemonId)
+ }
+
+ getPokemonTypeList():string[]{
+  return [
+    'Plante',
+    'Poison',
+    'Feu',
+    'Eau',
+    'Insecte',
+    'Normal',
+    'Vol',
+    'Electrik',
+    'Fée'
+  ]
+ }
+
 }
